3b9518c70cd6639b41a265c5f18a470ba48d9281
AbsoluteZer0527 <zhuluyuan200527@gmail.com>
2024-02-14 14:22:37 -0800

    yes

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,9 +8,54 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+cp student-submission/*.java grading-area
+cp *.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+if ! [ -f ListExamples.java ]
+then
+    echo "Missing ListExamples.java in student submission"
+    echo "Score: 0"
+    exit
+fi
+
+javac -cp $CPATH *.java
+if [ $? -ne 0 ]
+then 
+    echo "Compilation Error"
+    echo "Score: 0"
+    exit
+fi
+
+cp student-submission grading-area
+cp -r list-examples-grader grading-area
+
+javac -cp $CPATH TestListExamples.java ListExamples.java
+
+if [ $? -ne 0 ]
+then
+    echo "Error: Compilation failed."
+    echo "Score: 0"
+    exit 1
+fi
+
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+successes=$((tests - failures))
+echo "Score: $successes / $tests"
+
+rm -rf grading-area
+
+exit 0
---
+++ notes.md
@@ -0,0 +1,13 @@
+zhulu@Abs0 MINGW64 ~/OneDrive/Documents/GitHub/list-examples-grader (main)
+$ bash grade.sh https://github.com/ucsd-cse15l-f22/list-methods-lab3
+Cloning into 'student-submission'...
+remote: Enumerating objects: 3, done.        
+remote: Counting objects: 100% (3/3), done.  
+remote: Compressing objects: 100% (2/2), done.
+remote: Total 3 (delta 0), reused 3 (delta 0), pack-reused 0
+Receiving objects: 100% (3/3), done.
+Finished cloning
+cp: cannot stat 'student-submission': No such file or directory
+cp: cannot stat 'list-examples-grader': No such file or directory
+Score: 0 / 1
+
